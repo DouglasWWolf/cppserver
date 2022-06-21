3067c6d04840a351f060bb2c749b9a78a46cb41f
DouglasWWolf <douglaswolf@yahoo.com>
2022-06-20 19:09:45 -0700

    See History.h for changes

---
+++ server.h
@@ -0,0 +1,15 @@
+//==========================================================================================================
+// server.h - Defines a TCP command-line server
+//==========================================================================================================
+#pragma once
+#include "cmd_server_base.h"
+
+
+class CServer : public CCmdServerBase
+{
+public:
+
+    // Default constructor
+    CServer() : CCmdServerBase() {}
+
+}
